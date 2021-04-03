e19f4ae5f43dd4c6647425c5a46ec5bd1d49efd7
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-04-03 18:47:55 -0300

    Adição de um componente de edição de produto

--- src/components/cardProduct/CardProduct.jsx
+++ src/components/cardProduct/CardProduct.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import ContUnit from "../contUnit/ContUnit"
 import recycle_color_32 from "../../img/recycle_color_32.svg"
+import edit_color_24 from "../../img/edit_color_24.svg"
+import CardProductEdit from "./CardProductEdit"
 import "./cardProduct.css";
 
 const CardProduct = (props) => {
-    let iconRecycle = <img id="icon-recycle" src={recycle_color_32} alt="recycle_icon"></img>;
-
     const { informations } = props;
+    let iconRecycle = <img id="icon-recycle" src={recycle_color_32} alt="recycle_icon"></img>;
 
     if (!informations.recycle) {
         iconRecycle = "";
     }
 
-    return (
+    const cardDefault =
         <div className="card-product">
+            <button className="button_blue" id="button_edit" onClick={e => setCardProduct(<CardProductEdit actionCancel={{fun: setCardProduct, param: cardDefault}} informations={{ ...informations }} />)}><img style={{ verticalAlign: "middle" }} src={edit_color_24} alt="edit_icon"></img></button>
             <div className="div-img">
                 <img src={informations.img} alt="product_img"></img>
             </div>
@@ -24,6 +26,11 @@ const CardProduct = (props) => {
             <a href="/home">View details</a>
             <ContUnit max={informations.unit}></ContUnit>
         </div>
+
+    const [cardProduct, setCardProduct] = React.useState(cardDefault);
+
+    return (
+        cardProduct
     );
 }
 
